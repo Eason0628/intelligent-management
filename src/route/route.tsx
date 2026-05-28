18feae096c0e37ee627122a230146e7b1809f874
Eason0628 <zhaowintoo@gmail.com>
2026-05-28 20:15:05 +0800

    1.Initialize project & configure routing

---
+++ src/route/route.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+const Home = React.lazy(() => import("../page/Home"));
+const Login = React.lazy(() => import("../page/Login"));
+const NotFound = React.lazy(() => import("../page/404"));
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "*",
+        element: <NotFound />
+    }
+])
+export default router
