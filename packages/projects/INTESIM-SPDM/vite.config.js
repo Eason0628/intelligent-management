bd8bb89622985c9af725366267aea2820d551340
Eason0628 <zhaowintoo@gmail.com>
2026-05-29 17:16:58 +0800

    2.npm install vue-router & config routing

--- packages/projects/INTESIM-SPDM/vite.config.js
+++ packages/projects/INTESIM-SPDM/vite.config.js
@@ -1,7 +1,48 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import url from "url";
+import path, { dirname } from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+export default defineConfig({ //https://vite.dev/config/
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@common": path.resolve(__dirname, "../../common"),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 3000,
+    inline: false,
+    host: '0.0.0.0',
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (_proxyReq, req, _res) => {
+            console.log(`[Vite Proxy] 转发请求: ${req.url} -> ${options.target}${req.url}`)
+          })
+        },
+      },
+    },
+    build: {
+      outDir: path.resolve(__dirname, "dist"),
+      minify: "terser",
+      sourcemap: false,
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
+    },
+  },
+
+
 })
