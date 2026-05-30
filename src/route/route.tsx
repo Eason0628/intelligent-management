507bc2849feb4e7d6cde70405d34ad6e27dd9d99
Eason0628 <zhaowintoo@gmail.com>
2026-05-30 08:41:09 +0800

    3.1 Add Static Resources

--- src/route/route.tsx
+++ src/route/route.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 const Home = React.lazy(() => import("../page/Home"));
-const Login = React.lazy(() => import("../page/Login"));
+const Login = React.lazy(() => import("../page/login/Login"));
 const NotFound = React.lazy(() => import("../page/404"));
 const router = createBrowserRouter([
     {
