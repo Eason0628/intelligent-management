52317d858522bb2fff27c7404cb1519a21da8fb2
Eason0628 <zhaowintoo@gmail.com>
2026-05-30 08:26:07 +0800

    2.Configure lazy loading for routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -6,7 +7,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <Suspense fallback={<div>Loading...</div>}>
+    <App />
+  </Suspense>
 );
 
 
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,7 +1,11 @@
-function Home () {
+ cd import { Button } from 'antd';
+
+
+function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <Button type="primary">Primary Button</Button>
         </div>
     )
 }
